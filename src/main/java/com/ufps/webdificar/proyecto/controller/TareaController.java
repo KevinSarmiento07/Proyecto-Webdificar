bcbb91e732d0f89859962e966469fcaac09ea27e
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2023-06-14 04:54:16 -0500

    Agregando tareas

--- src/main/java/com/ufps/webdificar/proyecto/controller/TareaController.java
+++ src/main/java/com/ufps/webdificar/proyecto/controller/TareaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ufps.webdificar.proyecto.entities.Proyecto;
 import com.ufps.webdificar.proyecto.entities.Tarea;
+import com.ufps.webdificar.proyecto.entities.Trabajador;
 import com.ufps.webdificar.proyecto.repositories.ProyectoRepository;
 import com.ufps.webdificar.proyecto.repositories.TareaRepository;
+import com.ufps.webdificar.proyecto.repositories.TrabajadorRepository;
 
 import jakarta.validation.Valid;
 
@@ -29,8 +32,15 @@ public class TareaController {
 	@Autowired
 	private ProyectoRepository proyectoRepository;
 
-	@GetMapping
-	public String registro() {
+	@Autowired
+	private TrabajadorRepository trabajadorRepository;
+
+	@GetMapping("/registrar")
+	public String registro(@Valid Tarea tarea, Model model, BindingResult result) {
+		List<Trabajador> trabajadores = trabajadorRepository.findAll();
+		List<Proyecto> proyectos = proyectoRepository.findAll();
+		model.addAttribute("trabajadores", trabajadores);
+		model.addAttribute("proyectos", proyectos);
 		return "views/tareaRegistrar";
 	}
 	
@@ -39,15 +49,15 @@ public class TareaController {
 		
 		List<Tarea> tareas = tareaRepository.findAll();
 		model.addAttribute("tareas", tareas);
-		return "";// clocar vistar
+		return "views/proyectoEdit";// clocar vistar
 	}
 	
 	@Transactional
 	@PostMapping("/crear")
 	public String procesarFormCrearTarea(@Valid Tarea tarea, Model model){
-		Tarea tareaNueva = tareaRepository.save(tarea);
+		Tarea tareaNueva = tareaRepository.save(tarea);		
 		model.addAttribute("tarea", tareaNueva);
-		return "redirect:/tarea";
+		return "redirect:/proyecto/editar/"+tareaNueva.getProyecto().getId().toString();
 	}
 	
 	@GetMapping("/crear")
